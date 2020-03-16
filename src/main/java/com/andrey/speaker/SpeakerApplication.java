b751ddabc3a49fbe7a8fe8a085d685465cf6f1ca
AASh <AASh@mail.com>
2020-03-16 23:15:38 +0300

    initial commit

---
+++ src/main/java/com/andrey/speaker/SpeakerApplication.java
@@ -0,0 +1,13 @@
+package com.andrey.speaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpeakerApplication.class, args);
+	}
+
+}
