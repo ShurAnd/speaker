36eef69dadb28aaf2a4c04c931987a7f9d254009
AASh <AASh@mail.com>
2020-03-28 16:01:56 +0300

    fix problems with migration file

--- src/main/resources/db/migration/V1__init_DB.sql
+++ src/main/resources/db/migration/V1__init_DB.sql
@@ -1,23 +1,23 @@
-Hibernate: create sequence hibernate_sequence start 1 increment 1
-Hibernate: create table messages 
+create sequence hibernate_sequence start 1 increment 1;
+create table messages 
 					(id int8 not null,
 					filename varchar(255),
 					message varchar(2048) not null,
 					tag varchar(255),
 					user_id int8,
-					primary key (id))
-Hibernate: create table user_role 
+					primary key (id));
+create table user_role 
 					(user_id int8 not null,
-					roles varchar(255))
-Hibernate: create table usr 
+					roles varchar(255));
+create table usr 
 					(id int8 not null,
 					activation_code varchar(255),
 					active boolean not null,
 					mail varchar(255),
 					password varchar(255) not null,
 					username varchar(255) not null,
-					primary key (id))
-Hibernate: alter table if exists messages
-			add constraint message_usr_fk foreign key (user_id) references usr
-Hibernate: alter table if exists user_role
-			add constraint user_roles_usr_fk foreign key (usr_id) references usr
+					primary key (id));
+alter table if exists messages
+			add constraint message_usr_fk foreign key (user_id) references usr;
+alter table if exists user_role
+			add constraint user_roles_usr_fk foreign key (user_id) references usr;
